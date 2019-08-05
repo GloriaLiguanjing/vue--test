73c37f85df6e785270d7d07a2c6f3aa456b0988e
AAApeng <451039051@qq.com>
2019-08-05 15:56:09 +0800

    update Charts.vue

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,40 +1,147 @@
 <template>
-    <div id="container" ref="charts"  :style="{width: '300px',height:'300px'}"></div>
+    <div id="container" >
+		<div class="table" ref="charts"  :style="{width: '400px',height:'300px'}"></div>
+		<div class="table" ref="orgs"  :style="{width: '400px',height:'300px'}"></div>
+		<div class="table" ref="articles"  :style="{width: '800px',height:'300px'}"></div>
+	</div>
+  
 </template>
 <script>
+
 	export default{	
 		data(){
 			return{
-				msg:'Welcome'
+				msg:'Welcome',
+				data:[],
+				dataValue:[],
+				dataName:[],
+				dataAN:[],
+				dataAT:[]
 			}
 		},
 		mounted: function() {
-			this.drawLine();
+			this.initDate();
 		},
 		methods:{
+			 initDate(){
+				 //获取栏目
+			  this.getRequest('/article-categorys/tree').then(resp=>{
+				console.log(resp);
+				resp.data.forEach((item,index) => {					
+						var {childrenNumber,name}=item;
+						let v=item.childrenNumber;
+						let n=item.name;
+						var {value,name}={value:v,name:n}
+						this.data.push({value,name});
+				});
+			     this.drawLine();
+			  });
+			//获取组织机构
+			this.getRequest('/orgs').then(resp=>{
+				console.log(resp);
+				resp.data.forEach((item,index) => {	
+				
+						var {childrenNum,name}=item;
+						let s=item.childrenNum;
+						let l=item.name;
+						this.dataValue.push(s);
+						this.dataName.push(l);				
+						
+				});
+			     this.drawLineOrg();
+			});
+			//获取文章信息
+			//获取组织机构
+			this.getRequest('/articles').then(resp=>{
+
+				resp.data.forEach((item,index) => {	
+				    
+						var {upsNum,id}=item;
+						let s=item.upsNum;
+						let l=item.id;
+						this.dataAN.push(s);
+						this.dataAT.push(l);				
+						
+				});
+				console.log(this.dataAT);
+				console.log(this.dataAN);
+			     this.drawLineArticles();
+			})
+		  },
+		  //绘制栏目图
 		drawLine(){
 			// 基于准备好的dom，初始化echarts实例
 			var Chart = this.$refs.charts;
-			let myChart = this.$echarts.init(Chart)
-			
-           
+			var myChart = this.$echarts.init(Chart)
 			// 绘制图表
 			myChart.setOption({
-				title: { text: '在Vue中使用echarts' },
+				 title: { text: '栏目分布图' },
+				 series : [
+						{
+							name: '栏目统计',
+							type: 'pie',
+							radius: '55%',
+							roseType: 'angle',
+							data:this.data
+						}
+					]
+			});	
+		  },
+		 //绘制组织机构图
+         drawLineOrg(){
+			 // 基于准备好的dom，初始化echarts实例
+			var org = this.$refs.orgs;
+			var myOrgChart = this.$echarts.init(org)
+			// 绘制图表
+			myOrgChart.setOption({
+				title: {
+					text: '组织机构状态图'
+				},
 				tooltip: {},
 				xAxis: {
-					data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+					data: this.dataName
 				},
 				yAxis: {},
 				series: [{
-					name: '销量',
+					name: '数量',
 					type: 'bar',
-					data: [5, 20, 36, 10, 10, 20]
+					data: this.dataValue
 				}]
 			});
-		  }
+		 },
+		  drawLineArticles(){
+			 // 基于准备好的dom，初始化echarts实例
+			var articles = this.$refs.articles;
+			var myArticleChart = this.$echarts.init(articles);
+			// 绘制图表
+			myArticleChart.setOption({
+				title: {
+					text: '文章点赞统计图'
+				},
+				tooltip: {},
+				xAxis: {
+					data: this.dataAT
+				},
+				yAxis: {},
+				series: [{
+					name: '数量',
+					type: 'bar',
+					data: this.dataAN
+				}]
+			});
+		 }
 	   }
 	};
 </script>
 <style>
+#container{
+	width:90%;
+	background-color: #ffffff;
+	margin:5%;
+	padding:20px;
+}
+.table{
+	display:line-block;
+   width:45%;
+}
 </style>          
